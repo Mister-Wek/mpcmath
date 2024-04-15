7da8c0a343be31fefb48b3f56f9bed983f6ead9b
mrwek <68474288+Mister-Wek@users.noreply.github.com>
2024-04-15 10:42:38 +0300

    Create funcs.py

---
+++ lib/funcs.py
@@ -0,0 +1,22 @@
+prefix = 'math.'
+
+def math_ex(ex):
+    
+    i = 0
+    
+    result = 0
+    
+    ex = ['0','+'] + ex
+    
+    while i < len(ex):
+        
+        if ex[i] == '+':
+            result += int(ex[i+1])
+            
+        elif ex[i] == '-':
+            result -= int(ex[i+1])
+        
+        i += 1
+    return str(result)
+    
+funcs = {'ex':math_ex}
